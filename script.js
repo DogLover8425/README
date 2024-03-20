7b8e7993169db47fec4aa84d675178a869a63394
DogLover8425 <164244879+DogLover8425@users.noreply.github.com>
2024-03-20 17:02:51 -0400

    Update script.js
    Fixed 'reading properties of null' error

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function verify() {
     x = document.getElementById("container");
     x.remove();
     x = document.createElement("div");
+    document.body.appendChild(x);
     x.setAttribute("id","container");
     x = document.getElementById("container");
 }
